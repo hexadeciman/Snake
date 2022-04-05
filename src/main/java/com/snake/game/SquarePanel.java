6785f3bd7216dc1cdada13772e7264e46fa001da
Owen <ocrewe04@gmail.com>
2022-04-05 00:46:29 -0700

    Converted to maven project

---
+++ src/main/java/com/snake/game/SquarePanel.java
@@ -0,0 +1,19 @@
+package com.snake.game;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class SquarePanel extends JPanel{
+
+    private static final long serialVersionUID = 1L;
+
+    public SquarePanel(Color d){
+        this.setBackground(d);
+    }
+
+    public void ChangeColor(Color d){
+        this.setBackground(d);
+        this.repaint();
+    }
+
+}
---
+++ src/main/java/com/snake/game/Tuple.java
@@ -0,0 +1,31 @@
+package com.snake.game;
+
+public class Tuple {
+    public  int x;
+    public  int y;
+    public int xf;
+    public int yf;
+
+    public Tuple(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void ChangeData(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getXf(){
+        return xf;
+    }
+    public int getYf(){
+        return yf;
+    }
+}
