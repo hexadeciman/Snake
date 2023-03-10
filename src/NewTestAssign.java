5214a4da0b0f1cd88eeceed9cda6280cf7383631
RaiYan_163 <raiyanashraf20@gmail.com>
2023-03-10 11:05:32 +0600

    Assignment of lab of git

---
+++ src/NewTestAssign.java
@@ -0,0 +1,38 @@
+public class NewTestAssign
+{
+    private int test1;
+    private int test2;
+    private int test3;
+
+    public NewTestAssign(int test1, int test2, int test3) {
+        this.test1 = test1;
+        this.test2 = test2;
+        this.test3 = test3;
+    }
+
+    public void setTest1(int test1) {
+        this.test1 = test1;
+    }
+
+    public void setTest2(int test2) {
+        this.test2 = test2;
+    }
+
+    public void setTest3(int test3) {
+        this.test3 = test3;
+    }
+
+    public int getTest1() {
+        return test1;
+    }
+
+    public int getTest2() {
+        return test2;
+    }
+
+    public int getTest3() {
+        return test3;
+    }
+}
+
+//Cretated as an asisgnment by Raiyan Ashraf (1907023)
