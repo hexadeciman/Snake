c4564d85f1f95126b7ed663d1526129c99ebc40c
RaiYan_163 <raiyanashraf20@gmail.com>
2023-03-10 11:15:40 +0600

    New in branch (Assignment)

---
+++ src/NewInBranch.java
@@ -0,0 +1,25 @@
+public class NewInBranch {
+    int x;
+    int y;
+
+    public NewInBranch(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
